8d38477109884203886eeb700e098a326e85de4b
sattamap <sattamap@gmail.com>
2024-06-12 00:50:42 +0600

    Setup Express server with TypeScript and MongoDB configuration

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+// Welcome route
+app.get("/", (req: Request, res: Response) => {
+    res.send("server is running!");
+  });
+
+export default app;
