51a4376647ec6d5b49dbfc36b00494199397466f
sattamap <sattamap@gmail.com>
2024-06-12 18:36:45 +0600

    Update router name in  user.route.ts and response data in  user.controller.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
+import { userRoutes } from './app/modules/user/user.route';
+
 
 const app: Application = express();
 
 //parsers
 app.use(express.json());
 app.use(cors());
+
+
+// Register routes
+app.use('/api/auth', userRoutes);
 
 // Welcome route
 app.get("/", (req: Request, res: Response) => {
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -4,12 +4,22 @@ import { createUser } from './user.service';
 export const signup = async (req: Request, res: Response) => {
     try {
         const user = await createUser(req.body);
-        const { password, ...userData } = user; // Exclude password from response
+        
+        // Construct the response object with _id as the first field in data
+        const responseData = {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            phone: user.phone,
+            role: user.role,
+            address: user.address
+        };
+
         res.status(200).json({
             success: true,
             statusCode: 200,
             message: 'User registered successfully',
-            data: userData
+            data: responseData
         });
     } catch (error: any) {
         res.status(500).json({
