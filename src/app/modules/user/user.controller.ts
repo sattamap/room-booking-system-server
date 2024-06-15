edc762a68468e1290af65fae8ee4c3fcd5c44cbb
sattamap <sattamap@gmail.com>
2024-06-15 09:33:37 +0600

    Added two file catchAsync.ts and sendResponse.ts for code refactoring and refactor all files in user module

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,21 +1,15 @@
+// src/app/modules/user/user.controller.ts
 import { Request, Response } from 'express';
 import { createUser } from './user.service';
+import sendResponse from '../../utils/sendResponse';
+import catchAsync from '../../utils/catchAsync';
 
-export const signup = async (req: Request, res: Response) => {
-    try {
-        const user = await createUser(req.body);
-        res.status(200).json({
-            success: true,
-            statusCode: 200,
-            message: 'User registered successfully',
-            data: user // Directly use user as it does not include the password
-        });
-    } catch (error: any) {
-        res.status(500).json({
-            success: false,
-            statusCode: 500,
-            message: 'Error registering user',
-            error: error.message
-        });
-    }
-};
+export const signup = catchAsync(async (req: Request, res: Response) => {
+    const user = await createUser(req.body);
+    sendResponse(res, {
+        statusCode: 201,
+        success: true,
+        message: 'User registered successfully',
+        data: user,
+    });
+});
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,3 +1,4 @@
+// src/app/modules/user/user.route.ts
 import { Router } from 'express';
 import { signup } from './user.controller';
 
@@ -5,4 +6,4 @@ const router = Router();
 
 router.post('/signup', signup);
 
-export const userRoutes =router;
+export const userRoutes = router;
